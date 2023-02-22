1a91c14e02f9f517f2cef053a77eb40eefe43e79
Sergey Pristavko <serezha2145@gmail.com>
2023-02-22 08:43:26 +0300

    extension improvements

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,54 +1,69 @@
-import initToggleSubtitles from './toggleSubtitles'
-import initRewindButtons from './setRewindButtons'
-import initReplaceSubtitles from './replaceSubtitles'
-import {
-    BACK_BUTTON_ID,
-    DICTIONARY_BASE_ID,
-    FORWARD_BUTTON_ID,
-    PREV_QUOTE_ID,
-    REWIND_INTERVAL_ID,
-    SHOW_SUBS_ID,
-    TOGGLE_SUBS_ID,
-    getCachedValueOrDefault
-} from "../constants";
-import {EPISODES_LIST} from "./selectors";
+import '../styles/style.css'
+import copyIcon from '../assets/copy-icon.svg'
+import doneIcon from '../assets/done.png'
 
-// const episodesList = document.querySelector(EPISODES_LIST)
-//
-// console.log(episodesList)
-//
-// const episodeChangeObserver = new MutationObserver(() => {
-//     console.log('update')
-// })
-//
-// episodeChangeObserver.observe(document.querySelector(EPISODES_LIST), {
-//     attributes: true,
-//     childList: true,
-//     subtree: true
-// })
+const DONE_ICON_SHOWTIME = 1200
+const EXAMPLES_NUMBER = 3
+const SYNONYMS_NUMBER = 3
 
+const $entries = [
+    ...document.querySelectorAll('.pr.idiom-block'),
+    ...document.querySelectorAll('.pr.entry-body__el')
+]
 
-chrome.storage.sync.get([
-    BACK_BUTTON_ID,
-    DICTIONARY_BASE_ID,
-    PREV_QUOTE_ID,
-    REWIND_INTERVAL_ID,
-    SHOW_SUBS_ID,
-    TOGGLE_SUBS_ID
-]).then(userPreferences => {
-    initToggleSubtitles({
-        toggleSubsButton: getCachedValueOrDefault(userPreferences, TOGGLE_SUBS_ID),
-        showSubsButton: getCachedValueOrDefault(userPreferences, SHOW_SUBS_ID)
+const elementSelectedHandler = (element) => {
+    if (!element) return
+
+    element.classList.add('selected-by-cdc')
+}
+
+try {
+    $entries.forEach($entry => {
+        const $ = selector => {
+            const element = $entry.querySelector(selector)
+
+            if (element) {
+                elementSelectedHandler(element)
+                return element.textContent.trim()
+            }
+
+            return ''
+        }
+
+        const $$ = (selector, numberOfElements) => {
+            const elements = (Array.isArray(selector) ? selector : [selector])
+                .map(x => [...$entry.querySelectorAll(x)]).flat().slice(0, numberOfElements)
+
+            elements.forEach(elementSelectedHandler)
+            return [...new Set(elements.map(x => x.textContent.trim()))]
+        }
+
+        const headWord = $('.di-title')
+        const pronunciation = $('.us.dpron-i  .pron.dpron') || $('.pron.dpron')
+        const description = $('.def.ddef_d.db').replaceAll(':', '').trim()
+        // TODO: take more examples from the "More examples" section
+        const examples = $$('.examp.dexamp', EXAMPLES_NUMBER).join('\n')
+        const synonyms = $$([`.synonym a`, `.synonyms a`, `.daccord.fs16 a`], SYNONYMS_NUMBER).join(', ')
+
+        const onclick = () => {
+            const data = `${headWord} ${pronunciation} ${synonyms.length ? `(${synonyms})` : ''} \n${examples}\n\n${description}`
+
+            navigator.clipboard.writeText(data)
+
+            copyButton.src = doneIcon
+
+            setTimeout(() => {
+                copyButton.src = copyIcon
+            }, DONE_ICON_SHOWTIME)
+        }
+
+        const copyButton = document.createElement('img')
+        copyButton.src = copyIcon
+        copyButton.className = 'copy-btn'
+        copyButton.onclick = onclick
+
+        $entry.querySelector('.di-title').insertAdjacentElement('beforeEnd', copyButton)
     })
-
-    initRewindButtons({
-        back: getCachedValueOrDefault(userPreferences, BACK_BUTTON_ID),
-        forward: getCachedValueOrDefault(userPreferences, FORWARD_BUTTON_ID),
-        prevQuote: getCachedValueOrDefault(userPreferences, PREV_QUOTE_ID),
-        rewindInterval: getCachedValueOrDefault(userPreferences, REWIND_INTERVAL_ID),
-    })
-
-    initReplaceSubtitles({
-        dictionaryBase: getCachedValueOrDefault(userPreferences, DICTIONARY_BASE_ID)
-    })
-})
+} catch (e) {
+    console.error('fix the shit', e)
+}
--- src/scripts/utils.js
+++
@@ -1,6 +0,0 @@
-export const clearHTMLTags = str => str.replace(/(<([^>]+)>)/gi, ' ')
-
-export const createButtonKey = key => `Key${key.toUpperCase()}`
-
-export const findBySelectorAndLabel = (selector, label) =>
-    [...document.querySelectorAll(selector)].map(x => [...x.children]).flat().find(x => x.innerHTML.toLowerCase() === label)?.parentElement
