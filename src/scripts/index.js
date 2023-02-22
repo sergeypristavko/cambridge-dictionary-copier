97de14e1ece49fc9abc1c150b49648c54ed0327f
Sergey Pristavko <serezha2145@gmail.com>
2023-02-22 08:56:46 +0300

    self review

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,8 +3,8 @@ import copyIcon from '../assets/copy-icon.svg'
 import doneIcon from '../assets/done.png'
 
 const DONE_ICON_SHOWTIME = 1200
-const EXAMPLES_NUMBER = 3
-const SYNONYMS_NUMBER = 3
+const EXAMPLES_COUNT = 3
+const SYNONYMS_COUNT = 3
 
 const $entries = [
     ...document.querySelectorAll('.pr.idiom-block'),
@@ -41,8 +41,8 @@ $entries.forEach($entry => {
     const headWord = $('.di-title')
     const pronunciation = $('.us.dpron-i  .pron.dpron') || $('.pron.dpron')
     const description = $('.def.ddef_d.db').replaceAll(':', '').trim()
-    const examples = $$('.examp.dexamp', EXAMPLES_NUMBER).join('\n')
-    const synonyms = $$([`.synonym a`, `.synonyms a`, `.daccord.fs16 a`], SYNONYMS_NUMBER).join(', ')
+    const examples = $$('.examp.dexamp', EXAMPLES_COUNT).join('\n')
+    const synonyms = $$([`.synonym a`, `.synonyms a`, `.daccord.fs16 a`], SYNONYMS_COUNT).join(', ')
 
     const onclick = () => {
         const data = `${headWord} ${pronunciation} ${synonyms.length ? `(${synonyms})` : ''} \n${examples}\n\n${description}`
